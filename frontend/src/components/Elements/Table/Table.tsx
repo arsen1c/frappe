b9023da3fb6bd16d983bb1598c117b59263d04f1
arsen1c <vector2912@gmail.com>
2022-12-15 23:33:32 +0530

    feat: delete issue

--- frontend/src/components/Elements/Table/Table.tsx
+++ frontend/src/components/Elements/Table/Table.tsx
@@ -17,19 +17,13 @@ import {
 import { IconCheck, IconPencil, IconTrash } from '@tabler/icons';
 import { useState } from 'react';
 import { IIssue, useFetch } from '../../../hooks/useFetch';
+import { deleteRequest } from '../../../utils/AxiosInstance';
 
 
-const NotificationComponent = () => {
-    return (
-        <Notification icon={<IconCheck size={20} />} title="We notify you that">
-            You are now obligated to give a star to Mantine project on GitHub
-        </Notification>
-    )
-}
 
 function ModalContent({ issueId }: { issueId: string }) {
     return (
-        <Button color={"red"} onClick={NotificationComponent}>Delete</Button>
+        <Button color={"red"} >Delete</Button>
     )
 }
 
@@ -39,7 +33,27 @@ export default function IssuesTable() {
 
     const { data, error, isPending } = useFetch("/issue/all");
     const [deleteModal, setDeleteModal] = useState({ opened: false, issueId: "" });
+    const [issueIdSelected, setIssueIdSelected] = useState("");
+    const [userIdSelected, setUserIdSelected] = useState("");
     const [editModalOpened, setEditModalOpened] = useState(false);
+
+    const [issues, setIssues] = useState<IIssue[]>([]);
+
+    function deleteIssue(issueId: string, userId: string) {
+        deleteRequest("/user/issue", {
+            data: {
+                issueid: issueId,
+                userid: userId
+            }
+        }).then(res => {
+            const newIssues = issues.filter(issue => issue._id !== issueId);
+            setIssues([...newIssues]);
+        }).catch(err => {
+            console.log(err);
+            // error =  
+
+        })
+    }
 
     const rows = data && data.map((item: IIssue) => (
         <tr key={item._id}>
@@ -73,7 +87,12 @@ export default function IssuesTable() {
                         <IconPencil size={16} stroke={1.5} />
                     </ActionIcon>
                     <ActionIcon color="red">
-                        <IconTrash onClick={() => setDeleteModal({ opened: true, issueId: item._id })} size={16} stroke={1.5} />
+                        <IconTrash onClick={() => {
+                            setDeleteModal({ opened: true, issueId: item._id })
+                            setIssueIdSelected(item._id);
+                            setUserIdSelected(item.userId.id);
+                        }
+                        } size={16} stroke={1.5} />
                     </ActionIcon>
                 </Group>
             </td>
@@ -87,7 +106,9 @@ export default function IssuesTable() {
             {error && <Text color={"red"}>Couldn't fetch issues: {error}</Text>}
             {(data && !isPending) &&
                 <Center style={{ display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-                    <Modal centered opened={deleteModal.opened} withCloseButton={true} title={`Delete issue ${deleteModal.issueId}`} size="auto" onClose={() => setDeleteModal({ opened: false, issueId: "" })}>{<ModalContent issueId={"LMA"} />}</Modal>
+                    <Modal centered opened={deleteModal.opened} withCloseButton={true} title={`Delete issue ${deleteModal.issueId}`} size="auto" onClose={() => setDeleteModal({ opened: false, issueId: "" })}>{
+                        <Button color={"red"} onClick={() => deleteIssue(issueIdSelected, userIdSelected)}>Delete</Button>
+                    }</Modal>
                     <Button onClick={() => toggleColorScheme()}>New Issue</Button>
                     <Table highlightOnHover sx={{ minWidth: 800, maxHeight: "1px" }} verticalSpacing="xs">
                         <thead>
