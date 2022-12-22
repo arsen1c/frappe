81c8be6efa49757646978152e2c73ad9f73809ea
arsen1c <vector2912@gmail.com>
2022-12-23 00:25:36 +0530

    🎉 feat: member details update modal

---
+++ frontend/src/components/Elements/Modals/MemberModal.tsx
@@ -0,0 +1,34 @@
+import { Button, TextInput, Text } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { IconCurrencyRupee } from '@tabler/icons';
+import { IMember } from '../../../interfaces/Member.interface';
+import useSWRMutation from "swr/mutation";
+import { AxiosInstance } from '../../../utils/AxiosInstance';
+import { successToast } from '../../../utils/ToastNotifications';
+
+const sendRequest = async (url: string, ...args: any) => {
+    return AxiosInstance.put(url, args[0].arg);
+}
+
+function MemberModal({ member }: { member: IMember }) {
+    const form = useForm<IMember>({ initialValues: { ...member } })
+    const totalIssues = form.getInputProps("booksIssued");
+    const { trigger, isMutating, data, error } = useSWRMutation("/user", sendRequest);
+
+    if (data) {
+        successToast("Details Updated!")
+    }
+
+    return (
+        <div>
+            {error && <Text color={"red"}>{error.message}</Text>}
+            <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
+            <TextInput mt="md" label="Username" placeholder="Username" {...form.getInputProps('username')} />
+            <TextInput mt="md" label="Issues" placeholder="Issues" value={totalIssues.value.length} disabled={true} />
+            <TextInput icon={<IconCurrencyRupee />} mt="md" label="Debt" placeholder="Debt" {...form.getInputProps('debt')} />
+            <Button onClick={() => trigger(form.values)} my={20} loading={isMutating}>Save</Button>
+        </div>
+    )
+}
+
+export default MemberModal
--- frontend/src/components/Elements/Table/MemberTable.tsx
+++ frontend/src/components/Elements/Table/MemberTable.tsx
@@ -9,51 +9,50 @@ import {
     Title,
     Loader,
     Center,
-    Modal,
     Button,
     Notification,
     useMantineColorScheme,
     Menu,
+    Modal,
 } from '@mantine/core';
-import { IconCheck, IconCurrencyRupee, IconDots, IconPencil, IconTrash, IconUser } from '@tabler/icons';
+import { IconCurrencyRupee, IconDots, IconPencil, IconTrash } from '@tabler/icons';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useFetch } from '../../../hooks/useFetch';
-import { IBook } from '../../../interfaces/Book.interface';
 import { IMember } from '../../../interfaces/Member.interface';
 import { infoData } from '../Member/MemberInfoCard';
 import { MemberInfoICard } from '../Member/MemberInfoCard';
+import MemberModal from '../Modals/MemberModal';
 
-
-const NotificationComponent = () => {
-    return (
-        <Notification icon={<IconCheck size={20} />} title="We notify you that">
-            You are now obligated to give a star to Mantine project on GitHub
-        </Notification>
-    )
+interface IMemberModal {
+    open: boolean;
+    member: IMember;
 }
 
-function ModalContent({ issueId }: { issueId: string }) {
-    return (
-        <Button color={"red"} onClick={NotificationComponent}>Delete</Button>
-    )
+export const memberFormInitialValues: IMember = {
+    _id: "",
+    createdAt: "",
+    __v: 0,
+    debt: 0,
+    isAdmin: false,
+    name: "",
+    updatedAt: "",
+    username: "",
+    booksIssued: [0]
 }
 
 export default function MembersTable() {
     const theme = useMantineTheme();
-    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const [memberModalOpen, setMemberModalOpen] = useState<IMemberModal>({ open: false, member: memberFormInitialValues });
 
     const { data, error, isPending } = useFetch("/user/all");
-    const [deleteModal, setDeleteModal] = useState({ opened: false, issueId: "" });
-    const [editModalOpened, setEditModalOpened] = useState(false);
 
     const rows = data && data.map((member: IMember) => (
         <tr key={member._id}>
             <td>
-                <Link style={{ textDecoration: "none" }} to={`/member/${member.username}`}>
-                    <Text color={theme.black} size="sm" tt="capitalize" weight={500}>
-                        <MemberInfoICard {...infoData({ title: member.isAdmin ? "Admin" : "Member", name: member.name })} />
-                    </Text></Link>
+                <Text variant='link' color={theme.black} size="sm" tt="capitalize" weight={500}>
+                    <MemberInfoICard {...infoData({ title: member.isAdmin ? "Admin" : "Member", name: member.name })} />
+                </Text>
             </td>
             <td>
                 <Text size="sm" weight={500}>
@@ -78,7 +77,7 @@ export default function MembersTable() {
                         </ActionIcon>
                     </Menu.Target>
                     <Menu.Dropdown>
-                        <Menu.Item icon={<IconPencil size={16} stroke={1.5} />}>Edit</Menu.Item>
+                        <Menu.Item onClick={() => setMemberModalOpen({ open: true, member })} icon={<IconPencil size={16} stroke={1.5} />}>Edit</Menu.Item>
                         <Menu.Item icon={<IconTrash size={16} stroke={1.5} />} color="red">
                             Delete
                         </Menu.Item>
@@ -98,9 +97,9 @@ export default function MembersTable() {
                 }}
             >
                 <Title>Members</Title>
-                <Button sx={{ alignSelf: "end" }} onClick={() => toggleColorScheme()}>New Member</Button>
+                <Button sx={{ alignSelf: "end" }} onClick={() => null}>New Member</Button>
             </Group>
-            <Modal centered opened={deleteModal.opened} withCloseButton={true} title={`Delete issue ${deleteModal.issueId}`} size="auto" onClose={() => setDeleteModal({ opened: false, issueId: "" })}>{<ModalContent issueId={"LMA"} />}</Modal>
+            <Modal title="Edit member" opened={memberModalOpen.open} onClose={() => setMemberModalOpen({ open: false, member: memberFormInitialValues })}><MemberModal member={memberModalOpen.member} /></Modal>
             <ScrollArea>
                 {isPending && <Center style={{ margin: 100 }}><Loader variant='dots' size={"xl"} /></Center>}
                 {error && <Text color={"red"}>Couldn't fetch members: {error}</Text>}
