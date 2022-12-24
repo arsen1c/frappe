1672f30cfce5ac9b6075a8e14b6e6611be3a0030
arsen1c <vector2912@gmail.com>
2022-12-24 21:15:45 +0530

    🎉 feat: delete book modal

---
+++ frontend/src/components/Elements/Modals/DeleteBookModal.tsx
@@ -0,0 +1,30 @@
+import { Button, Center, Text } from "@mantine/core";
+import { AxiosInstance } from "../../../utils/AxiosInstance";
+import useSWRMutation from "swr/mutation";
+import { successToast } from "../../../utils/ToastNotifications";
+import { deleteRequest } from "../../../utils/AxiosInstance";
+
+interface PropType {
+    bookId: number;
+}
+
+const sendDeleteRequest = async (endpoint: string, ...args: any) => {
+    return deleteRequest(endpoint, args[0].arg);
+}
+
+function DeleteBookModal({ bookId }: PropType) {
+    const { trigger, data, error, isMutating } = useSWRMutation(`/book/delete/${bookId}`, sendDeleteRequest);
+
+    if (data) {
+        successToast("Book deleted!");
+    }
+
+    return (
+        <div>
+            {error && <Center><Text color={"red"}>{error.message}</Text></Center>}
+            <Button loading={isMutating} onClick={() => trigger({ bookId })} color="red">Delete</Button>
+        </div>
+    )
+}
+
+export default DeleteBookModal;
--- frontend/src/components/Elements/Table/BooksTable.tsx
+++ frontend/src/components/Elements/Table/BooksTable.tsx
@@ -23,6 +23,7 @@ import useSWRMutation from "swr/mutation";
 import { errorToast, successToast } from '../../../utils/ToastNotifications';
 import { useDebouncedValue } from '@mantine/hooks';
 import { BookSearchModal } from '../Modals/BookSearchModal';
+import DeleteBookModal from '../Modals/DeleteBookModal';
 
 const fetcher = (url: string) => getRequest<IBook[]>(url).then(res => res.data);
 const importFetcher = (url: string) => getRequest<IBook[]>(url).then(res => res.data);
@@ -33,6 +34,8 @@ export default function BooksTable() {
     const { trigger, error: importError, data: importBooksData, isMutating } = useSWRMutation("/book/import", importFetcher);
 
     const [searchModalOpen, setSearchModalOpen] = useState(false);
+    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+    const [bookSelected, setBookSelected] = useState(0);
 
     if (importBooksData) {
         successToast("Books imported succesfully!");
@@ -77,7 +80,10 @@ export default function BooksTable() {
                     </Menu.Target>
                     <Menu.Dropdown>
                         <Menu.Item icon={<IconPencil size={16} stroke={1.5} />}>Edit</Menu.Item>
-                        <Menu.Item icon={<IconTrash size={16} stroke={1.5} />} color="red">
+                        <Menu.Item icon={<IconTrash size={16} stroke={1.5} />} color="red" onClick={() => {
+                            setDeleteModalOpen(true);
+                            setBookSelected(item.bookID)
+                        }}>
                             Delete
                         </Menu.Item>
                     </Menu.Dropdown>
@@ -110,6 +116,7 @@ export default function BooksTable() {
                 </Group>
             </Group>
             <Modal fullScreen opened={searchModalOpen} onClose={() => setSearchModalOpen(false)} size={"xl"}><BookSearchModal /></Modal>
+            <Modal opened={deleteModalOpen} onClose={() => setDeleteModalOpen(false)} size={"lg"} title={`Delete book ${bookSelected}`}><DeleteBookModal bookId={bookSelected} /></Modal>
             <ScrollArea>
                 {isLoading && <Center style={{ margin: 100 }}><Loader variant='dots' size={"xl"} /></Center>}
                 {error && <Text color={"red"}>Couldn't fetch books: {error}</Text>}
