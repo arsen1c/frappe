1216cef03a63a3a5cb705922e875ed8a2bf126f7
arsen1c <vector2912@gmail.com>
2022-12-25 02:16:28 +0530

    cleanup

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,7 +1,4 @@
 import IssuesTable from '../Elements/Table/Table'
-import { IIssue, useFetch } from '../../hooks/useFetch';
-import { useEffect } from 'react';
-import { useState } from 'react';
 
 function Home() {
 
